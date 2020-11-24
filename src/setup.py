4625756b7582c218d477e629951c29001422f334
David A. Dahlbom <david.dahlbom@gmail.com>
2020-11-23 19:54:34 -0500

    added submodule, updated makefile to be platform agnostic

--- src/setup.py
+++ src/setup.py
@@ -4,8 +4,11 @@ The Cython build script.  Called from Makefile
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
+import os
 
-ext = Extension("pycarfac", sources=["pycarfac.pyx", "../ccarfac/carfac.c"])
+ccarfacloc = os.path.join(os.curdir, "..", "ccarfac", "src")
+
+ext = Extension("pycarfac", sources=["pycarfac.pyx", os.path.join(ccarfacloc, "carfac.c")])
 setup(name="pycarfac", ext_modules=cythonize([ext]),
-        include_dirs=[np.get_include(), "../ccarfac/"])
+        include_dirs=[np.get_include(), ccarfacloc])
 
